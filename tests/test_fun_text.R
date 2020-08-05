453ce4ae7fa128a3619ce8de9efa51339c080e2a
John Bradley <johnbradley2008@gmail.com>
2020-08-05 08:13:05 -0400

    fun_text tests use realistic data

--- tests/test_fun_text.R
+++ tests/test_fun_text.R
@@ -17,31 +17,31 @@ pathways <- tibble(go=go, pathway=pathway, gene=gene) %>%
 source(here::here("code/fun_text.R"), local=TRUE)
 
 test_that("summary_gene pulls approved_symbol from gene_summary by default", {
-  data_params <- list(id="RDX")
+  data_params <- list(type="gene", id="RDX", gene_symbols=c("RDX"))
   result <- summary_gene(input=data_params)
   expect_equal(result, "RDX")
 })
 
 test_that("summary_gene pulls approved_name from gene_summary when specified", {
-  data_params <- list(id="RDX")
+  data_params <- list(type="gene", id="RDX", gene_symbols=c("RDX"))
   result <- summary_gene(input=data_params, var="approved_name")
   expect_equal(result, "Radixin")
 })
 
 test_that("summary_pathway pulls pathway from pathways by default", {
-  data_params <- list(id="123456")
+  data_params <- list(type="pathway", id="123456", gene_symbols=c("RDX"))
   result <- summary_pathway(input=data_params)
   expect_equal(result, "My Protein")
 })
 
 test_that("summary_pathway pulls data seperated by commas from pathways when specified", {
-  data_params <- list(id="123456")
+  data_params <- list(type="pathway", id="123456", gene_symbols=c("RDX"))
   result <- summary_pathway(input=data_params, var="data")
   expect_equal(result, "RDX, ROCK2")
 })
 
 test_that("summary_gene_list filters out invalid symbols from gene_summary", {
-  data_params <- list(gene_symbols=c("RDX","ROCK2","OTHER"))
+  data_params <- list(type="gene_list", id="RDX,ROCK2,OTHER", gene_symbols=c("RDX","ROCK2","OTHER"))
   result <- summary_gene_list(input=data_params)
   expect_equal(result, "RDX, ROCK2")
 })
