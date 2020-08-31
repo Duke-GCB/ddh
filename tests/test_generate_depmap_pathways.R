218894ff3ffa4a508b3f32753439c740b5b587b8
John Bradley <johnbradley2008@gmail.com>
2020-08-31 10:00:26 -0400

    test cast_enrichr_data

--- tests/test_generate_depmap_pathways.R
+++ tests/test_generate_depmap_pathways.R
@@ -10,3 +10,14 @@ test_that("enrichr_loop with empty gene_list can be arranged by Adjusted.P.value
     arrange(Adjusted.P.value)
   expect_equal(nrow(result), 0)
 })
+
+test_that("cast_enrichr_data casts Term, Overlap and Genes", {
+  result = cast_enrichr_data(tibble(
+    Term=1:3,
+    Overlap=2:4,
+    Genes=3:5,
+  ))
+  expect_equal(result$Term, c("1","2","3"))
+  expect_equal(result$Overlap, c("2","3","4"))
+  expect_equal(result$Genes, c("3","4","5"))
+})
