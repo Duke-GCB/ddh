f8ed24f8943cf2d36797fcee2b7b100645a29409
Ben Neubert <bcn6fe@virginia.edu>
2020-12-16 13:05:16 -0500

    Removed visNetwork webshot implementation in DDH reports. Restored ggraph for report image generation.

--- code/fun_reports.R
+++ code/fun_reports.R
@@ -58,32 +58,9 @@ render_rmarkdown_in_tempdir <- function(data_values, rmd_path, output_file, envi
   network <- get("network", envir = envir)  
   network_filename <- paste0(good_file_name, "_", "graph")
   
-  # Create and save zoomed version of network so that the network works well with webshot
-  networkZoomed <- network %>% visEvents(stabilizationIterationsDone = "function() {
-     this.moveTo({scale:2})}") 
-  visSave(networkZoomed, file = paste0(network_filename, "Zoomed.html"))
-  
-  # Save base non-zoomed network with legend added for the final zip
+  # Save base network with legend added for the final zip
   visSave(network %>% visLegend(position = "right", width = .25, zoom = F), file = paste0(network_filename, "Interactive.html"))
   zip_filenames <- append(zip_filenames, paste0(network_filename, "Interactive.html"))
-  
-  # Create picture of the network
-  webshot::webshot(url =  paste0(network_filename, "Zoomed.html"), file = paste0(network_filename,".png"), vwidth = 3000, vheight = 3000, zoom = 1, delay = 5, cliprect = c(250,400,2300,2300))
-  
-  # Add legend to the network picture
-  networkImage <- image_read(paste0(network_filename,".png"))
-  legendImage <- image_read(here("visNetworkLegendHorizontal.png"))
-  combinedImage <- c(networkImage, legendImage) %>% 
-    image_append(stack = T) %>% 
-    image_write(paste0(network_filename,".png"))
-  
-  # Remove the zoomed network
-  file.remove(paste0(network_filename, "Zoomed.html"))
-  
-  zip_filenames <- append(zip_filenames, paste0(network_filename, ".png"))
-  
-  # assign filename information to envir so the network image can be found within the report_gene.Rmd file
-  assign("networkImagePath", paste0(network_filename,".png"), envir = envir)
 
   rmarkdown::render(rmd_filename, output_file = output_pdf_filename, envir = envir)
   # get the names of all the items included for rendering
@@ -113,8 +90,11 @@ render_rmarkdown_in_tempdir <- function(data_values, rmd_path, output_file, envi
       if (name == "cellanatogram") {
         ggsave(plot_filename, env_item, width = 8, height = 7, dpi = 300, type = "cairo")
       }
+      if (name == "graph") {
+        ggsave(plot_filename, env_item, width = 9, height = 7, dpi = 300, type = "cairo")
+      }
       # landscape aspect ratio for all other plots
-      if (!name %in% c("lineage", "sublineage", "cellbins", "cellanatogram")) {
+      if (!name %in% c("lineage", "sublineage", "cellbins", "cellanatogram", "graph")) {
         ggsave(plot_filename, env_item, width = 12, height = 7.5, dpi = 300, type = "cairo")
       }
       # include the plot png in the zip download
@@ -148,6 +128,7 @@ render_gene_report <- function(data_values, output_file) {
   dep_bottom <- make_bottom_table(bottomtable_data = master_bottom_table, gene_symbol)
   flat_bottom_complete <- make_enrichment_bottom(enrichmentbottom_data = master_negative, gene_symbol)
   network <- make_graph(toptable_data = master_top_table, bottomtable_data = master_bottom_table, gene_symbol, threshold = 10, deg = 2, corrType = "Positive and Negative", displayHeight = '80vh', displayWidth = '100%')
+  graph <- make_graph_report(toptable_data = master_top_table, bottomtable_data = master_bottom_table, gene_symbol)
   render_rmarkdown_in_tempdir(data_values, here::here("code", "report_gene.Rmd"), output_file)
 }
 
